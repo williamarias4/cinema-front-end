48e225a30975cd457873b7fe2caa517ddfdebaa9
William Arias <39339999+williamarias4@users.noreply.github.com>
2022-05-31 03:02:48 -0600

    Added a constant config file for variables.

---
+++ assets/js/config.js
@@ -0,0 +1,6 @@
+const moviedb = {
+    base_url: "https://api.themoviedb.org/3/movie/",
+    api_key: "04f47ebfbae6bd56a395b86cef4f2d58",
+    api_query_param: "?api_key=",
+    movie_ids_path: "public/movie_ids.json"
+}
