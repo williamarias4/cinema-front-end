73c7389e3bb72395042efc5d21ee8fdf71781da7
William Arias <39339999+williamarias4@users.noreply.github.com>
2022-07-09 16:10:53 -0600

    Added module imports, added debounce function to navbar.

--- assets/js/nav-hamburguer.js
+++ assets/js/nav-hamburguer.js
@@ -1,19 +1,23 @@
+
+import { debounce } from '/assets/js/throttle.js'
+
+
 (function ($) {
     $(function () {
-      $("nav ul li > a:not(:only-child)").click(function (e) {
+      $("nav ul li > a:not(:only-child)").click(debounce(function (e) {
         $(this).siblings(".nav-dropdown").slideToggle();
         $(".nav-dropdown").not($(this).siblings()).hide();
         e.stopPropagation();
-      });
-      $("html").click(function () {
+      }));
+      $("html").click(debounce(function () {
         $(".nav-dropdown").hide();
-      });
+      }));
       // Toggle open and close nav styles on click
-      $("#nav-toggle").click(function () {
+      $("#nav-toggle").click(debounce(function () {
         $("nav ul").slideToggle();
-      });
-      $("#nav-toggle").on("click", function () {
+      }));
+      $("#nav-toggle").on("click", debounce(function () {
         this.classList.toggle("active");
-      });
+      }));
     });
   })(jQuery);
