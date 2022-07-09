8c1d12617a8d18a8bdd629ae9d6f3b4e67de472b
William Arias <39339999+williamarias4@users.noreply.github.com>
2022-07-09 16:07:54 -0600

    Movie poster size adjusted to solve img size error

--- assets/js/config.js
+++ assets/js/config.js
@@ -1,6 +1,6 @@
 const moviedb = {
     base_url: "https://api.themoviedb.org/3/movie/",
-    base_img_url: "https://image.tmdb.org/t/p/original/",
+    base_img_url: "https://image.tmdb.org/t/p/w780/",
     api_key: "04f47ebfbae6bd56a395b86cef4f2d58",
     api_query_param: "?api_key=",
     movie_ids_path: "public/movie_ids.json"
