196581a8b2e08c35cfda2ecdd3ebccb68f6428cb
William Arias <39339999+williamarias4@users.noreply.github.com>
2022-07-13 16:53:39 -0600

    Added Module method for fetch.

---
+++ assets/js/fetch-method.js
@@ -0,0 +1,52 @@
+function getFetch(url, callback) {
+    fetch(url, {
+        method: "GET",
+        headers: {
+            "Content-Type": "application/json"
+        },
+    }).then(response => response.json()
+    ).then(data => {
+        //console.log(info)
+        callback(data)
+        //console.log(info.id)
+        //console.log(info.original_title)
+    }), function (error) { //switch to arrow
+        console.log(error);
+    }
+}
+
+function postJSON(url, callback, method, body) {
+    fetch(url, {
+        method: method,
+        body: body,
+        headers: {
+            "Content-Type": "application/json"
+        }
+
+    }).then(response => response.json()
+    ).then(data => callback(data)
+    ), function (error) { //switch to arrow
+        console.log(error);
+    }
+}
+
+
+/*fetch(url, {
+    method: "POST",
+    body: JSON.stringify(data),
+    headers: {
+      "Content-Type": "application/json"
+    },
+    credentials: "same-origin"
+  }).then(function(response) {
+    response.status     //=> number 100–599
+    response.statusText //=> String
+    response.headers    //=> Headers
+    response.url        //=> String
+  
+    return response.text()
+  }, function(error) {
+    error.message //=> String
+  })*/
+
+export default getFetch;
