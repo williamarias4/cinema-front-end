7327421ccad9c335e35debe0e89e9f0bc8d927f7
William Arias <39339999+williamarias4@users.noreply.github.com>
2022-07-17 15:04:01 -0600

    Adjusted responsiveness for mobile.

--- assets/js/style-element.js
+++ assets/js/style-element.js
@@ -16,6 +16,11 @@ let style_mouseout = (img) => {
     return function () {
         isMouseOverElement = false;
         if (isMouseOverElement === false) {
+            if (window.matchMedia('(max-device-width: 700px)').matches) {
+                img.style.width = "95%";
+                img.style.opacity = "100%";
+                return;
+            }
             img.style.width = "90%";
             img.style.opacity = "100%";
         }
@@ -28,4 +33,4 @@ function style_outside_element_onhover(element_to_hover, element_to_style) {
 
 }
 
-export {style_outside_element_onhover, style_on_hover, style_mouseout};
+export { style_outside_element_onhover, style_on_hover, style_mouseout };
