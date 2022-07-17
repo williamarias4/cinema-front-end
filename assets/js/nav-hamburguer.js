af024d7e0c218aa49cd327e6238ca7128b21980d
William Arias <39339999+williamarias4@users.noreply.github.com>
2022-07-17 14:05:40 -0600

    Debounce function bugs navbar, disabled temporary.

--- assets/js/nav-hamburguer.js
+++ assets/js/nav-hamburguer.js
@@ -4,19 +4,19 @@ import { debounce } from '/assets/js/throttle.js'
 
 (function ($) {
     $(function () {
-      $("nav ul li > a:not(:only-child)").click(debounce(function (e) {
+      $("nav ul li > a:not(:only-child)").click(/*debounce*/(function (e) {
         $(this).siblings(".nav-dropdown").slideToggle();
         $(".nav-dropdown").not($(this).siblings()).hide();
         e.stopPropagation();
       }));
-      $("html").click(debounce(function () {
+      $("html").click(/*debounce*/(function () {
         $(".nav-dropdown").hide();
       }));
       // Toggle open and close nav styles on click
-      $("#nav-toggle").click(debounce(function () {
+      $("#nav-toggle").click(/*debounce*/(function () {
         $("nav ul").slideToggle();
       }));
-      $("#nav-toggle").on("click", debounce(function () {
+      $("#nav-toggle").on("click", /*debounce*/(function () {
         this.classList.toggle("active");
       }));
     });
