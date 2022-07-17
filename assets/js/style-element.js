dae4cae1caffcd3299ddd01c8a9484e3c885b339
William Arias <39339999+williamarias4@users.noreply.github.com>
2022-07-17 13:59:12 -0600

    Created a new JS file for styling elements on hover.

---
+++ assets/js/style-element.js
@@ -0,0 +1,31 @@
+var isMouseOverElement = false;
+
+let style_on_hover = (img) => {
+
+    return function () {
+        isMouseOverElement = true;
+        if (isMouseOverElement === true) {
+            img.style.width = "100%";
+            img.style.opacity = "20%";
+        }
+    };
+}
+
+let style_mouseout = (img) => {
+
+    return function () {
+        isMouseOverElement = false;
+        if (isMouseOverElement === false) {
+            img.style.width = "90%";
+            img.style.opacity = "100%";
+        }
+    }
+}
+
+function style_outside_element_onhover(element_to_hover, element_to_style) {
+    element_to_hover.addEventListener('mouseover', style_on_hover(element_to_style));
+    element_to_hover.addEventListener('mouseout', style_mouseout(element_to_style));
+
+}
+
+export {style_outside_element_onhover, style_on_hover, style_mouseout};
